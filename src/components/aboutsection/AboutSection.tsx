49672a198dfaf12a0045fa5414de7be6d0af1bad
arjunisonlinee <arjunisonlinee@gmail.com>
2026-03-16 23:24:33 +0530

    analytics

--- src/components/aboutsection/AboutSection.tsx
+++ src/components/aboutsection/AboutSection.tsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
 import { IoLocation } from "react-icons/io5";
 
